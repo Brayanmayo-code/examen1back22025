72a788c93340373343fdbbe218cc42f5daea4bf2
brayanmayo-code <cdbrayanmayo154@gmail.com>
2025-08-18 20:02:53 -0500

    se agregan los cambios de esta carpeta

--- examen1back22025-main/examen1back22025-main/src/main/java/com/example/Examen1Back2/modelos/Usuario.java
+++ examen1back22025-main/examen1back22025-main/src/main/java/com/example/Examen1Back2/modelos/Usuario.java
@@ -2,12 +2,12 @@ package com.example.Examen1Back2.modelos;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
-@Entit
+@Entity
 @Table(name = "usuarios")
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Colun(name = "id_usuario")
     private Integer id;
 
@@ -39,5 +39,69 @@ public class Usuario {
         this.tipoUsuario = tipoUsuario;
     }
 
+    public Usuario(Integer id, String nombre, String correoElectronico, String contraseña, String telefono, TipoUsuario tipoUsuario, Docente docente) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correoElectronico = correoElectronico;
+        this.contraseña = contraseña;
+        this.telefono = telefono;
+        this.tipoUsuario = tipoUsuario;
+        this.docente = docente;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public TipoUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Docente getDocente() {
+        return docente;
+    }
+
+    public void setDocente(Docente docente) {
+        this.docente = docente;
+    }
 }
--- examen1back22025-main/examen1back22025-main/src/main/java/com/example/Examen1Back2/modelos/readme.txt
+++ examen1back22025-main/examen1back22025-main/src/main/java/com/example/Examen1Back2/modelos/readme.txt
@@ -15,5 +15,25 @@ Faltaba private a la linea Docente docente y el ;
 Y faltaba el metodo constructor
 
 
+DOCENTE.JAVA =
+En esta carpeta los cambios que hice fueron:
 
+Estaba mal escrito la anotacion @Entity estaba de esta manera @Entit
+
+Falbataba el @id en la linea 10 arriba de la linea id
+
+faltaba el constructor vacio y el constructor lleno
+
+faltaban los getters y setters para cursos y usuarios solo habia para id y especialidad
+
+
+USUARIOS.JAVA =
+En esta carpeta estabal mal copiado el @Entity estaba de esta manera @Entit
+
+en la linea 10 la  faltaba terminar de copiarlo le faltaba el IDENTITY
+
+El el @column estaba mal escrito estaba de esta manera @Colun
+
+Faltaban los gueters y setters para los atributos
+Y el construtor
 
